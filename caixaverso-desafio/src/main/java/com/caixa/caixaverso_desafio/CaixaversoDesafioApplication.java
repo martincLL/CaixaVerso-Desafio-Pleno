06c81a7f082626ef15b7efa74d0b12ce6d25a625
Mateus Montagnini <mateus.r.montagnini@gmail.com>
2025-11-15 19:02:46 -0300

    Primeiro commit

---
+++ caixaverso-desafio/src/main/java/com/caixa/caixaverso_desafio/CaixaversoDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.caixa.caixaverso_desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaixaversoDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaixaversoDesafioApplication.class, args);
+	}
+
+}
---
+++ caixaverso-desafio/src/test/java/com/caixa/caixaverso_desafio/CaixaversoDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caixa.caixaverso_desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaixaversoDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
